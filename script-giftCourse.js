d5070f79733b527ce65ebeedf7ac3db6f7da66e9
Hassan-Akbad <akbad091@gmail.com>
2026-04-09 19:57:35 +0100

    some JS changes

--- script-giftCourse.js
+++ script-giftCourse.js
@@ -2,13 +2,16 @@ const boxs = document.querySelectorAll(".box");
 
 boxs.forEach(box => {
     const checkbox = box.querySelector("input[type='checkbox']");
-    box.addEventListener("mouseover", function () {
-        box.classList.toggle("border-[#FF6636]")
-    });
-    box.addEventListener("mouseout", function () {
-        box.classList.toggle("border-[#FF6636]")
-    });
     checkbox.addEventListener("click", function () {
         box.classList.toggle("border-[#FF6636]")
     });
+});
+const check = document.getElementById("checkBox");
+const cardInfo = document.getElementById("cardInfo");
+check.addEventListener("change", function () {
+    if (check.checked) {
+        cardInfo.classList.remove('hidden');
+    } else {
+        cardInfo.classList.add('hidden');
+    }
 });
